9eea28aba552f067e51fa9425936c52208804f19
Joscelyn Gainié <joscelyn.gainie@gmail.com>
2021-01-12 10:51:33 +0100

    minor commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,7 @@
 #!/bin/sh -l
 
-echo "Wesh $1"
-echo "$(ls -la /)"
-echo "$(git status)"
-echo "$(git remote -v)"
-echo "git remote add main ssh://$1@$2$3"
 git remote add main ssh://$1@$2$3
-# git add .
-# git commit -m "minor commit"
+git add .
+git commit -m "minor commit"
 # git config core.sshCommand "ssh -i /key.pem -F /dev/null"
 # git push main master
