8f177ed6c87c770709e935131601c0b5978f101e
Joscelyn Gainié <joscelyn.gainie@gmail.com>
2021-01-12 10:42:54 +0100

    minor commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,13 @@ echo "$(ls -la /)"
 echo "$(git status)"
 echo "$(git remote -v)"
 echo "git remote add main ssh://$1@$2$3"
-git remote add main ssh://$1@$2$3
+# git remote add main ssh://$1@$2$3
 
 echo "dsqhidqsgyudqsyu"
 echo "$4"
 echo "dsqhidqsgyudqsyu2"
 echo $4 >> "/key.pem"
-git add .
-git commit -m "minor commit"
-git config core.sshCommand "ssh -i /key.pem -F /dev/null"
-git push main master
+# git add .
+# git commit -m "minor commit"
+# git config core.sshCommand "ssh -i /key.pem -F /dev/null"
+# git push main master
