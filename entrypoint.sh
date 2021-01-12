86375da60eea241719f8a05cf4ef3b839f27706b
Joscelyn Gainié <joscelyn.gainie@gmail.com>
2021-01-12 12:21:28 +0100

    minor commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ git config user.name "artfluens"
 git config user.email "contact@artfluens.com"
 git config core.sshCommand "ssh -o StrictHostKeyChecking=no -v -i /artfluens-ecdsa"
 
-git push main master --force
+git push main master
