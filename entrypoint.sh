ba7f67f62afb10659eba6635e5296ca8a4987ee9
Joscelyn Gainié <joscelyn.gainie@gmail.com>
2021-01-12 10:53:08 +0100

    minor commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -l
 
 git remote add main ssh://$1@$2$3
+git config user.name "artfluens"
+git config user.email "contact@artfluens.com"
 git add .
 git commit -m "minor commit"
 # git config core.sshCommand "ssh -i /key.pem -F /dev/null"
