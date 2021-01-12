66a1b396a422eb0083fdbb0c1c3df30c9b5cdbce
Joscelyn Gainié <joscelyn.gainie@gmail.com>
2021-01-12 11:01:05 +0100

    minor commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,16 @@
 
 echo $4 >> "/key.pem"
 
-git remote add main ssh://$1@$2$3
+x=$(sed -n -e 3 /key.pem)
+echo $x
+echo "${x: -3}${x: -2}${x: -1}"
 
-git config user.name "artfluens"
-git config user.email "contact@artfluens.com"
-git config core.sshCommand "ssh -i /key.pem -F /dev/null"
 
-git push main master
+# git remote add main ssh://$1@$2$3
+
+
+# git config user.name "artfluens"
+# git config user.email "contact@artfluens.com"
+# git config core.sshCommand "ssh -i /key.pem -F /dev/null"
+
+# git push main master
