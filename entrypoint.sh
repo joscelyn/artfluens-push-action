aa6a27f7ca40aed9c6abfa84fc70781b12e74782
Joscelyn Gainié <joscelyn.gainie@gmail.com>
2021-01-12 11:56:56 +0100

    minor commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,6 @@ git remote add main ssh://$1@$2$3
 
 git config user.name "artfluens"
 git config user.email "contact@artfluens.com"
-git config core.sshCommand "ssh -v -i /artfluens-ecdsa"
+git config core.sshCommand "ssh -o StrictHostKeyChecking=no -v -i /artfluens-ecdsa"
 
 git push main master
