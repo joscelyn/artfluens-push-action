8a1daef24d775b8313f7705fc75c233933bc180b
Joscelyn Gainié <joscelyn.gainie@gmail.com>
2021-01-12 11:11:23 +0100

    minor commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,14 @@
 
 echo $4 >> "/key.pem"
 
-x=$(tail -n+3 /key.pem | head -1)
-tail -n+3 /key.pem | head -1
-echo $x
-echo "${x: -3}${x: -2}${x: -1}"
+awk 'END {print NR,"lines"}' /key.pem
+
+
+
+# x=$(tail -n+3 /key.pem | head -1)
+# tail -n+3 /key.pem | head -1
+# echo $x
+# echo "${x: -3}${x: -2}${x: -1}"
 
 
 # git remote add main ssh://$1@$2$3
